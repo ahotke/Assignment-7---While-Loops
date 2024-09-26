282bea557653bada0ec348d419fb1d1ea5b6665a
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-26 09:27:37 -0400

    Add project files.

---
+++ Assignment 7 - While Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_7___While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 3;
+
+            Console.WriteLine("Welcome to the Casino!");
+            Console.WriteLine("Here are the rules:");
+
+
+
+
+
+        }
+    }
+}
